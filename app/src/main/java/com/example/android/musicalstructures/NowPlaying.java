051f0e2d85a68c7ae906a38d89ce78ce0681ea26
alanionita <a.g.ionita@gmail.com>
2018-05-02 00:25:06 +0100

    adds a NowPlaying screen + class, and updates the PodcastAdaptor to include an onclicklistener on the inflated episode play button so that clicking it triggers the NowPlaying screen

---
+++ app/src/main/java/com/example/android/musicalstructures/NowPlaying.java
@@ -0,0 +1,17 @@
+package com.example.android.musicalstructures;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alanionita on 01/05/2018.
+ */
+
+public class NowPlaying extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.now_playing);
+        }
+}
